somathias/cbmos

commit b08e09ffc8081bfeec2a11bead33650c83e3911b

Avoid computing normalization twice

cbmos_serial.py
@@ -15,8 +15,8 @@ class CBMSolver:
         def f(t, y):
             y_r = y.reshape((-1, 3))
             tmp = np.repeat(y_r[:, :, np.newaxis], y_r.shape[0], axis=2)
-            normalization = np.sqrt(((tmp - tmp.transpose())**2).sum(axis=1)) + np.diag(np.ones(y_r.shape[0]))
-            forces = self.force(np.sqrt(((tmp - tmp.transpose())**2).sum(axis=1)), **force_args)/normalization
+            normalization = np.sqrt(((tmp - tmp.transpose())**2).sum(axis=1))
+            forces = self.force(normalization)/(normalization + np.diag(np.ones(y_r.shape[0])))
             total_force = (np.repeat(forces[:, np.newaxis, :], 3, axis=1)*(tmp.transpose()-tmp)
                     ).sum(axis=2)
             return (NU*total_force).reshape(-1)

commit 902309b935ffa942c2523ae1ba861fe483044f59

Restore force parameters
Fix bug introduced by b08e09f

cbmos_serial.py
@@ -18,7 +18,7 @@ class CBMSolver:
             y_r = y.reshape((-1, 3))
             tmp = np.repeat(y_r[:, :, np.newaxis], y_r.shape[0], axis=2)
             norm = np.sqrt(((tmp - tmp.transpose())**2).sum(axis=1))
-            forces = self.force(norm)/(norm
+            forces = self.force(norm, **force_args)/(norm
                                        + np.diag(np.ones(y_r.shape[0])))
             total_force = (np.repeat(forces[:, np.newaxis, :], 3,
                                      axis=1)*(tmp.transpose()-tmp)).sum(axis=2)