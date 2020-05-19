6fb70319c34db421fdd750506b5e4135903b7d0e
Sonja Mathias <sonja.mathias@it.uu.se>
2020-05-19 11:58:02 +0200

    Merge the global adaptivity code

--- euler_forward.py
+++ euler_forward.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
 
-def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.01):
+def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, eps=0.001, eta=0.01, out=''):
 
 
     t0, tf = float(t_span[0]), float(t_span[-1])
@@ -31,9 +31,24 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.01):
 
     ts = [t]
     ys = [y]
+    dts = []
+
+    adaptive_dt = True if dt is None else False
 
     while t < tf :
-        y = y + dt*fun(t,y)
+
+        if adaptive_dt:
+            # choose time step adaptively
+            F = fun(t,y)
+            norm_AF = np.linalg.norm(1/eta*(fun(t, y + eta * F) - F), np.inf)
+            #print('AF'+ str(AF))
+            dt = np.sqrt(2*eps/norm_AF) if norm_AF > 0.0 else tf - t
+            #print('dt' + str(dt))
+
+            y = y + dt*F
+        else:
+            y = y + dt*fun(t,y)
+
         t = t + dt
 
 #        if t_eval is not None:
@@ -52,9 +67,22 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.01):
 #        else:
         ts.append(t)
         ys.append(y)
+        dts.append(dt)
+#        print('len(ts) '+ str(len(ts)))
+#        print('len(ys) '+ str(len(ys)))
+#        print('len(dts) '+ str(len(dts)))
 
     ts = np.hstack(ts)
+#    print('len(ts) '+ str(len(ts)))
     ys = np.vstack(ys).T
+#    print('len(ys.T) '+ str(len(ys.T)))
+    dts = np.hstack(dts)
+#    print('len(dts) '+ str(len(dts)))
+
+    with open('time_points'+out+'.txt', 'ab') as f:
+        np.savetxt(f, ts[1:])
+    with open('step_sizes'+out+'.txt', 'ab') as f:
+        np.savetxt(f, dts)
 
     return OdeResult(t=ts, y=ys)
 
@@ -76,7 +104,14 @@ if __name__ == "__main__":
     t_eval = np.linspace(0,1,10)
     y0 = np.array([1])
 
-    sol = solve_ivp(func, [t_eval[0], t_eval[-1]], y0, t_eval=t_eval)
+    sol = solve_ivp(func, [t_eval[0], t_eval[-1]], y0, t_eval=None, dt=0.01, eps=0.001 )
+    plt.figure()
+    plt.plot(sol.t, sol.y.T)
+    plt.plot(sol.t, sol.y.T, '.', color='black')
+
+    sol2 = solve_ivp(func, [t_eval[0], t_eval[-1]], y0, t_eval=None, eps=0.001 )
+    plt.plot(sol2.t, sol2.y.T)
+    plt.plot(sol2.t, sol2.y.T, '*', color='red')
 
 
 
