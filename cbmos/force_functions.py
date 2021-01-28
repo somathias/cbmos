Fix force derivatives and add force arguments to Jacobian call in EB

cbmos/force_functions.py
@@ -205,8 +205,9 @@ class Logarithmic:
         def fp(r, mu=1.0, s=1.0):
             if r is None:
                 return 0.
-        r[r==0] = 0.0001  # get away from zero - this is an awful hack!
-        return _np.where(r < s, mu/(1+(r-s)), 0.)
+            r[r==0] = 0.0001  # get away from zero - this is an awful hack!
+            return _np.where(r < s, mu/(1+(r-s)), 0.)
+        return fp
 
 # linear-logarithmic
 class LinearLogarithmic:
@@ -231,8 +232,9 @@ class LinearLogarithmic:
         def fp(r, mu=1.0, s=1.0):
             if r is None:
                 return 0.
-        r[r==0] = 0.0001  # get away from zero - this is an awful hack!
-        return _np.where(r < s, -mu*np.log(1+(r-s))-mu*(r-s)/(1+(r-s)), 0.)
+            r[r==0] = 0.0001  # get away from zero - this is an awful hack!
+            return _np.where(r < s, -mu*_np.log(1+(r-s))-mu*(r-s)/(1+(r-s)), 0.)
+        return fp
 
 
 # hard-core model
@@ -299,11 +301,12 @@ class Gls:
         return _np.where(r < s, mu*_np.log(1+(r-s)), _np.where(r < rA, mu*(r-s)*_np.exp(-a*(r-s)), 0))
 
     def derive(self):
-        def fp(r, mu=1.0, s=1.0):
+        def fp(r, mu=1.0, s=1.0, a=5.0, rA=1.5):
             if r is None:
                 return 0.
-        r[r==0] = 0.0001  # get away from zero - this is an awful hack! Plus it does not allow for single value evaluation
-        return _np.where(r < s, mu/(1+(r-s)), _np.where(r < rA, mu*(1-a*(r-s))*np.exp(-a*(r-s)), 0))
+            r[r==0] = 0.0001  # get away from zero - this is an awful hack! Plus it does not allow for single value evaluation
+            return _np.where(r < s, mu/(1+(r-s)), _np.where(r < rA, mu*(1-a*(r-s))*_np.exp(-a*(r-s)), 0))
+        return fp
 
 
 

cbmos/solvers/euler_backward.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 import os
 plt.style.use('seaborn')
 
-def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, n_newton=2, jacobian=None,
+def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, n_newton=2, jacobian=None, force_args={},
               out='', write_to_file=False):
     # do regular fixed time stepping
     t0, tf = float(t_span[0]), float(t_span[-1])
@@ -37,7 +37,7 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, n_newton=2, jacobian=None,
         y_next = copy.deepcopy(y) # initialize with current y
         for j in np.arange(n_newton):
             if jacobian is not None:
-                A = jacobian(y_next)
+                A = jacobian(y_next, force_args)
             else:
                 print('Error: No jacobian provided!')
             J = np.eye(A.shape[0]) - dt*A