Fix call to Jacobian in adaptivity code in EF as well as tests and delete writing of time points to file

cbmos/solvers/euler_forward.py
@@ -13,7 +13,13 @@ plt.style.use('seaborn')
 
 def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, eps=0.01, eta=0.001,
               out='', write_to_file=False,
-              local_adaptivity=False, m0=2, m1=2, jacobian=None):
+              local_adaptivity=False, m0=2, m1=2,
+              jacobian=None, force_args={}):
+    """
+    Note: t_eval can only be taken into account when dt is provided and thus
+    fixed time stepping is done.
+    """
+
 
     adaptive_dt = True if dt is None else False
 
@@ -25,7 +31,8 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, eps=0.01, eta=0.001,
         else:
             return _do_local_adaptive_timestepping_ws(fun, t_span, y0, eps,
                                                       eta, out, write_to_file,
-                                                      m0, m1, jacobian)
+                                                      m0, m1, jacobian,
+                                                      force_args)
     elif adaptive_dt:
         # choose time step adaptively globally
         return _do_global_adaptive_timestepping(fun, t_span, y0, eps, eta, out,
@@ -33,7 +40,7 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, eps=0.01, eta=0.001,
     else:
         # do regular fixed time stepping
         t0, tf = float(t_span[0]), float(t_span[-1])
-        
+
         if t_eval is not None:
             assert t0 == t_eval[0]
             assert tf == t_eval[-1]
@@ -48,7 +55,6 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, eps=0.01, eta=0.001,
 
         ts = [t]
         ys = [y]
-        dts = []
 
         while t < tf:
 
@@ -73,18 +79,9 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, eps=0.01, eta=0.001,
             else:
                 ts.append(t)
                 ys.append(y)
-                dts.append(dt)
 
         ts = np.hstack(ts)
         ys = np.vstack(ys).T
-        dts = np.hstack(dts)
-
-
-        if write_to_file:
-            with open('time_points'+out+'.txt', 'ab') as f:
-                np.savetxt(f, ts)
-            with open('step_sizes'+out+'.txt', 'ab') as f:
-                np.savetxt(f, dts)
 
         return OdeResult(t=ts, y=ys)
 
@@ -126,8 +123,6 @@ def _do_global_adaptive_timestepping(fun, t_span, y0, eps, eta,
 
 
     if write_to_file:
-        with open('time_points'+out+'.txt', 'ab') as f:
-            np.savetxt(f, ts)
         with open('step_sizes'+out+'.txt', 'ab') as f:
             np.savetxt(f, dts)
 
@@ -242,8 +237,6 @@ def _do_local_adaptive_timestepping(fun, t_span, y0, eps, eta,
     n_eq_per_level = np.vstack(n_eq_per_level).T
 
     if write_to_file:
-        with open('time_points'+out+'.txt', 'ab') as f:
-            np.savetxt(f, ts)
         with open('step_sizes'+out+'.txt', 'ab') as f:
             np.savetxt(f, dts)
         with open('step_sizes_local'+out+'.txt', 'ab') as f:
@@ -257,7 +250,7 @@ def _do_local_adaptive_timestepping(fun, t_span, y0, eps, eta,
 
 def _do_local_adaptive_timestepping_ws(fun, t_span, y0, eps, eta,
                                        out, write_to_file,
-                                       m0, m1, jacobian):
+                                       m0, m1, jacobian, force_args):
 
     t0, tf = float(t_span[0]), float(t_span[-1])
 
@@ -276,7 +269,7 @@ def _do_local_adaptive_timestepping_ws(fun, t_span, y0, eps, eta,
         y = copy.deepcopy(y)
 
         # calculate stability bound
-        A = jacobian(y)
+        A = jacobian(y, force_args)
         w, v = np.linalg.eigh(A)
 
         # the eigenvalues are sorted in ascending order
@@ -394,8 +387,6 @@ def _do_local_adaptive_timestepping_ws(fun, t_span, y0, eps, eta,
     n_eq_per_level = np.vstack(n_eq_per_level).T
 
     if write_to_file:
-        with open('time_points'+out+'.txt', 'ab') as f:
-            np.savetxt(f, ts)
         with open('step_sizes'+out+'.txt', 'ab') as f:
             np.savetxt(f, dts)
         with open('step_sizes_local'+out+'.txt', 'ab') as f:
@@ -415,7 +406,7 @@ if __name__ == "__main__":
     #@np.vectorize
     def func(t, y):
         return -50*np.eye(len(y))@y
-    def jacobian(y):
+    def jacobian(y, fa):
         return -50*np.eye(len(y))
 
 #    t_span = (0,1)
@@ -504,5 +495,5 @@ if __name__ == "__main__":
     plt.plot(sorted_AFs[-2,:], label='$t=t_f$')
     plt.plot(sorted_AFs[-1,:], label='$t=t_f$')
     plt.xlabel('k')
-    plt.ylabel('$|\eta_k|$, sorted decreasingly')
+    #plt.ylabel('$|\eta_k|$, sorted decreasingly')
     plt.legend()

tests/test_euler_forward.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 18 16:45:08 2019
 
-@author: Sonja Mathias
-"""
 import numpy as np
 
 import cbmos.solvers.euler_forward as ef
@@ -20,7 +16,7 @@ def test_y_shape():
     t_span = (0, 1)
     y0 = np.array([1, 1])
 
-    sol = ef.solve_ivp(func, t_span, y0)
+    sol = ef.solve_ivp(func, t_span, y0, dt=0.01)
 
     assert len(sol.t) == 101
     assert sol.y.shape == (2, 101)
@@ -29,11 +25,25 @@ def test_t_eval():
     t_eval = np.linspace(0,1,10)
     y0 = np.array([1, -1])
 
-    sol = ef.solve_ivp(func, [t_eval[0], t_eval[-1]], y0, t_eval=t_eval)
+    sol = ef.solve_ivp(func, [t_eval[0], t_eval[-1]], y0, t_eval=t_eval, dt=0.01)
     assert len(sol.t) == len(t_eval)
 
     t_eval = np.linspace(0,1,1000)
     y0 = np.array([1, -1])
 
-    sol = ef.solve_ivp(func, [t_eval[0], t_eval[-1]], y0, t_eval=t_eval)
+    sol = ef.solve_ivp(func, [t_eval[0], t_eval[-1]], y0, t_eval=t_eval, dt=0.01)
     assert len(sol.t) == len(t_eval)
+
+def test_jacobian_arguments():
+    # test that local adaptivity code in EF can handle the correct signature
+    # of the Jacobian with the force function arguments
+    def func(t, y):
+        return -50*np.eye(len(y))@y
+    def jacobian(y, fa):
+        return -50*np.eye(len(y))
+
+    t_eval = np.linspace(0,3,10)
+    y0 = np.array([0.5, 1.0])
+
+    sol = ef.solve_ivp(func, [t_eval[0], t_eval[-1]], y0, t_eval=None,
+                       local_adaptivity=True, jacobian=jacobian)