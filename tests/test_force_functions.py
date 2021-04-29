2462c3a8979980ea9e9b16ccfb8326f93a550405
Sonja Mathias <sonja.mathias@it.uu.se>
2021-04-29 10:48:57 +0200

    Fix bug in linear exponential force and add test

--- tests/test_force_functions.py
+++ tests/test_force_functions.py
@@ -22,3 +22,13 @@ def test_gls():
 #    assert ff.gls(1.5) == 0.
 #    assert ff.gls(0.5) == - np.log(2)
 #    assert ff.gls(1.25) == 0.25*np.exp(-5.0*0.25)
+
+def test_linearexponential():
+    x = np.array([1.0, 1.5, 0.5, 1.25])
+    y = np.array([0., 0., - 7.5*np.exp(2.5), 15.0*0.25*np.exp(-5.0*0.25)])
+    assert (ff.LinearExponential()(x) == y).all()
+
+def test_linearexponential_derivative():
+    x = np.array([1.0, 1.5, 0.5, 1.25])
+    y = np.array([15.0, 0., 15.0*3.5*np.exp(2.5), -15.0*0.25*np.exp(-5.0*0.25)])
+    assert (ff.LinearExponential().derive()(x) == y).all()
