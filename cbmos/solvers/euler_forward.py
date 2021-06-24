7c8fef0842bf7e656ad686804541977d96db72da
Sonja Mathias <sonja.mathias@it.uu.se>
2021-06-24 13:48:28 +0200

    Add flag to EF to fix number of eqs (no update)

--- cbmos/solvers/euler_forward.py
+++ cbmos/solvers/euler_forward.py
@@ -14,7 +14,7 @@ plt.style.use('seaborn')
 def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, eps=0.01, eta=0.001,
               out='', write_to_file=False,
               local_adaptivity=False, m0=2, m1=2,
-              jacobian=None, force_args={}):
+              jacobian=None, force_args={}, fix_eqs=0):
     """
     Note: t_eval can only be taken into account when dt is provided and thus
     fixed time stepping is done.
@@ -47,7 +47,8 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=None, eps=0.01, eta=0.001,
                                                                    out,
                                                                    write_to_file,
                                                                    jacobian,
-                                                                   force_args)
+                                                                   force_args,
+                                                                   fix_eqs)
     else:
         # do regular fixed time stepping
         return _do_fixed_timestepping(fun, t_span, y0, t_eval, dt)
@@ -161,7 +162,8 @@ def _do_global_adaptive_timestepping(fun, t_span, y0, eps, eta,
 def _do_global_adaptive_timestepping_with_stability(fun, t_span, y0, eps,
                                                     out, write_to_file,
                                                     jacobian,
-                                                    force_args):
+                                                    force_args,
+                                                    fix_eqs):
 
     _logging.debug("Using EF, global adaptive time stepping with Jacobian and eps={}".format(
             eps))
@@ -214,6 +216,9 @@ def _do_global_adaptive_timestepping_with_stability(fun, t_span, y0, eps,
 
         # take minimum of dt and tf-t in order to not overstep
         dt = np.minimum(dt, tf-t)
+
+        if fix_eqs > 0:
+            F[:fix_eqs] = 0.0
 
         y = y + dt*F
         t = t + dt
