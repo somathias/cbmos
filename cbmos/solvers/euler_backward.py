b1db2996afa935b44a61cc0ce399e9154175b161
Sonja Mathias <sonja.mathias@it.uu.se>
2021-08-19 12:56:47 +0200

    Implemented global adaptivity in EB (accuracy bound only)

--- cbmos/solvers/euler_backward.py
+++ cbmos/solvers/euler_backward.py
@@ -9,6 +9,7 @@ from scipy.sparse.linalg import cg
 import scipy as scpi
 import copy
 from scipy.sparse.linalg import LinearOperator
+import logging as _logging
 
 
 import matplotlib.pyplot as plt
@@ -16,18 +17,10 @@ import os
 plt.style.use('seaborn')
 
 
-def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.1, n_newton=20,
-              eps=None, eps_max =1e-3, xi=0.001, jacobian=None, force_args={},
-              tol=None, atol=None,
+def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.1, eps=0.01, eta=0.001,
+              n_newton=20, eps_newton=None, eps_max =1e-3, xi=0.001,
+              jacobian=None, force_args={}, tol=None, atol=None,
               out='', write_to_file=False, disp=False):
-
-    if eps is None:
-        eps = min(eps_max, dt)
-    if tol is None:
-        tol = min(eps_max, dt)
-    if atol is None:
-        atol = min(eps_max, dt)
-
 
     class gmres_counter(object):
         def __init__(self, disp=disp):
@@ -37,6 +30,39 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.1, n_newton=20,
             self.niter += 1
             if self._disp:
                 print('iter %3i\trk = %s' % (self.niter, str(rk)))
+
+    counter = gmres_counter()
+
+
+    adaptive_dt = True if dt is None else False
+
+    if adaptive_dt:
+        # choose time step adaptively globally
+       return _do_global_adaptive_timestepping(fun, t_span, y0, t_eval,
+                                                    dt, eps, eta, n_newton,
+                                                    eps_newton, xi, jacobian,
+                                                    force_args, counter,
+                                                    eps_max, tol,
+                                                    atol, out, write_to_file)
+
+    else:
+        # do regular fixed time stepping
+        if eps_newton is None:
+            eps_newton = min(eps_max, dt)
+        if tol is None:
+            tol = min(eps_max, dt)
+        if atol is None:
+            atol = min(eps_max, dt)
+        return _do_fixed_timestepping(fun, t_span, y0, t_eval, dt,
+                                      n_newton, eps_newton, xi, jacobian,
+                                      force_args, counter, tol, atol, out,
+                                      write_to_file)
+
+def _do_fixed_timestepping(fun, t_span, y0, t_eval, dt, n_newton,
+                           eps_newton, xi, jacobian, force_args, counter,
+                           tol, atol, out, write_to_file):
+    _logging.debug("Using EB, fixed time stepping with dt={}".format(
+            dt))
 
     # do regular fixed time stepping
     t0, tf = float(t_span[0]), float(t_span[-1])
@@ -53,10 +79,7 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.1, n_newton=20,
         # take minimum of dt and tf-t in order to not overstep
         dt = np.minimum(dt, tf-t)
 
-        if disp:
-            print('--------')
-            print('t = '+str(t))
-            print('--------')
+        _logging.debug("t={}".format(t))
 
         y = copy.deepcopy(y)
 
@@ -78,21 +101,18 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.1, n_newton=20,
                 J = LinearOperator((len(y_next), len(y_next)), matvec=Jv)
 
             # solve linear system J*dy = F_curly for dy
-            counter = gmres_counter()
             dy, exitCode = gmres(J, -F_curly, callback=counter, tol=tol,
                                  atol=atol, restart=5, maxiter=1,
                                  callback_type='x') # maxiter= number of outer iterations/restarts, restart= number of inner iterations (between restarts)
-            if disp:
-                print('Number of GMRes iterations = '+str(counter.niter))
+            _logging.debug("Number of GMRes iterations = {}".format(counter.niter))
             #dy, exitCode = lgmres(J, -F_curly, callback=counter)
             #dy, exitCode = cg(J, -F_curly)
             # dy = scpi.linalg.solve(J, -F_curly)
             #print('ExitCode='+str(exitCode))
             y_next = y_next + dy
 
-            if np.linalg.norm(dy)/np.linalg.norm(y_next) < eps:
-                if disp:
-                    print('Relative error tolerance of '+str(eps)+' achieved.')
+            if np.linalg.norm(dy)/np.linalg.norm(y_next) < eps_newton:
+                _logging.debug("Relative error tolerance of {} achieved.".format(eps_newton))
                 break
 
         y = copy.deepcopy(y_next)
@@ -103,6 +123,101 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.1, n_newton=20,
         dts.append(dt)
 
 
+    ts = np.hstack(ts)
+    ys = np.vstack(ys).T
+    dts = np.hstack(dts)
+
+
+    if write_to_file:
+        with open('step_sizes'+out+'.txt', 'ab') as f:
+            np.savetxt(f, dts)
+
+    return OdeResult(t=ts, y=ys)
+
+def _do_global_adaptive_timestepping(fun, t_span, y0, t_eval, dt, eps, eta,
+                                     n_newton, eps_newton, xi, jacobian,
+                                     force_args, counter, eps_max, tol, atol,
+                                     out, write_to_file):
+    _logging.debug("Using EB, adaptive time stepping.")
+
+    t0, tf = float(t_span[0]), float(t_span[-1])
+
+    t = t0
+    y = y0
+
+    ts = [t]
+    ys = [y]
+    dts = []
+
+    while t < tf:
+
+        _logging.debug("t={}".format(t))
+
+        y = copy.deepcopy(y)
+
+        F = fun(t, y)
+        if jacobian is not None:
+            A = jacobian(y, force_args)
+            AF = A@F
+        else:
+            AF = 1/eta*(fun(t, y + eta * F) - F)
+
+        if write_to_file:
+            with open('AFs'+out+'.txt', 'ab') as f:
+                np.savetxt(f, np.abs(AF).reshape((1, -1)))
+
+        norm_AF = np.linalg.norm(AF, np.inf)
+        dt = np.sqrt(2*eps/norm_AF) if norm_AF > 0.0 else tf - t
+
+        # take minimum of dt and tf-t in order to not overstep
+        dt = np.minimum(dt, tf-t)
+
+        if eps_newton is None:
+            eps_newton = min(eps_max, dt)
+        if tol is None:
+            tol = min(eps_max, dt)
+        if atol is None:
+            atol = min(eps_max, dt)
+
+        # do Newton iterations
+        y_next = copy.deepcopy(y)  # initialize with current y
+        for j in np.arange(n_newton):
+
+            F_curly = y_next - y - dt*fun(t, y_next)
+
+            if jacobian is not None:
+                A = jacobian(y_next, force_args)
+                J = np.eye(A.shape[0]) - dt*A
+            else:
+                # approximate matrix vector product Jv where J = I-dt*A
+                def Jv(v):
+                    return 1/xi*(y_next + xi*v
+                                  - y - dt*fun(t, y_next + xi*v)
+                                  - F_curly)
+                J = LinearOperator((len(y_next), len(y_next)), matvec=Jv)
+
+            # solve linear system J*dy = F_curly for dy
+            dy, exitCode = gmres(J, -F_curly, callback=counter, tol=tol,
+                                 atol=atol, restart=5, maxiter=1,
+                                 callback_type='x') # maxiter= number of outer iterations/restarts, restart= number of inner iterations (between restarts)
+            _logging.debug("Number of GMRes iterations = {}".format(counter.niter))
+            #dy, exitCode = lgmres(J, -F_curly, callback=counter)
+            #dy, exitCode = cg(J, -F_curly)
+            # dy = scpi.linalg.solve(J, -F_curly)
+            #print('ExitCode='+str(exitCode))
+            y_next = y_next + dy
+
+            if np.linalg.norm(dy)/np.linalg.norm(y_next) < eps:
+                _logging.debug("Relative error tolerance of {} achieved.".format(eps))
+                break
+
+        y = copy.deepcopy(y_next)
+        t = t + dt
+
+        ts.append(t)
+        ys.append(y)
+        dts.append(dt)
+
 
     ts = np.hstack(ts)
     ys = np.vstack(ys).T
@@ -110,8 +225,6 @@ def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.1, n_newton=20,
 
 
     if write_to_file:
-        with open('time_points'+out+'.txt', 'ab') as f:
-            np.savetxt(f, ts)
         with open('step_sizes'+out+'.txt', 'ab') as f:
             np.savetxt(f, dts)
 
@@ -143,14 +256,10 @@ if __name__ == "__main__":
         os.remove('step_sizes.txt')
     except FileNotFoundError:
         print('Nothing to delete.')
-    try:
-        os.remove('time_points.txt')
-    except FileNotFoundError:
-        print('Nothing to delete.')
 
     #sol2 = solve_ivp(func, [t_eval[0], t_eval[-1]], y0, dt=0.1, n_newton = 2,
     #                 write_to_file=True, jacobian=jac)
-    sol2 = solve_ivp(func, [t_eval[0], t_eval[-1]], y0, dt=0.1, n_newton = 2,
+    sol2 = solve_ivp(func, [t_eval[0], t_eval[-1]], y0, dt=None, n_newton = 2,
                      write_to_file=True)
     #plt.plot(sol2.t, sol2.y.T)
     plt.plot(sol2.t, sol2.y.T, '*')
