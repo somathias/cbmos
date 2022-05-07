8a4eda175cfbb77dbfc0dac9ed0000efb36721fb
Sonja Mathias <sonja.mathias@it.uu.se>
2022-05-07 16:57:44 +0200

    Add data from rackham

---
+++ experiments/scripts/launch_linear_tissue_growth_05.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#SBATCH -A snic2021-22-607 -t 24:00:00 -p core -n 2    
+
+python3 exp-linear_tissue_growth_script.py ../data/20220506_rackham 16 0.5
+python3 exp-linear_tissue_growth_script.py ../data/20220506_rackham 15 0.5
+python3 exp-linear_tissue_growth_script.py ../data/20220506_rackham 14 0.5
+python3 exp-linear_tissue_growth_script.py ../data/20220506_rackham 13 0.5
