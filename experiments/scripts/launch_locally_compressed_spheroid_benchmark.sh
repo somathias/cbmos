e55a934a791a885c33c1ea377ec7d3c13bef68eb
Sonja Mathias <sonja.mathias@it.uu.se>
2022-05-10 16:58:30 +0200

    Prepare to launch locally_compressed_spheroid benchmark on rackham

--- experiments/scripts/launch_locally_compressed_spheroid_benchmark.sh
+++ experiments/scripts/launch_locally_compressed_spheroid_benchmark.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#SBATCH -A snic2021-22-607 -t 2:00:00 -p core -n 2    
+#SBATCH -A snic2021-22-607 -t 24:00:00 -p core -n 2    
 
-python3 exp-locally_compressed_spheroid_benchmark.py exp_locally_compressed_spheroid_benchmark_s0 0
+python3 exp-locally_compressed_spheroid_benchmark.py ../data/20220510_rackham_size13_eps0.005_m14 13 0.005 14
